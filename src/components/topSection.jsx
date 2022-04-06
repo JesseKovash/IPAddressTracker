80318024f6493ae8f9de4a12674462cfb08c3931
Jesse Kovash <jessekovash@gmail.com>
2022-04-05 18:39:41 -0600

    top section completed. next step is ipdetails, and map

--- src/components/topSection.jsx
+++ src/components/topSection.jsx
@@ -4,10 +4,13 @@ import Button from '@material-ui/core/Button';
 
 function TopSection(props) {
   return (
-    <div id="topSection" style={{ backgroundImage: 'url("../../../images/pattern-bg.png")'}}>
+    <div id="topSection" style={{ backgroundImage: 'url("../../../images/pattern-bg.png")' }}>
       <h2>IP Address Tracker</h2>
-      <input type="text"></input>
-      <button>></button>
+      <div id="input_container">
+        <input type="text" placeholder="Search for any IP address or domain" id="IP_input"></input>
+        <button id="IP_input_button"><img src="../../../images/icon-arrow.svg"></img></button>
+      </div>
+
     </div>
 
 
